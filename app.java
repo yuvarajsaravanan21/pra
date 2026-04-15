c88b762627f68af226661be693bcdf878c3bc40f
yuvarajsaravanan21 <yuvaraj.saravanan2023@vitstudent.ac.in>
2026-04-15 22:09:44 +0530

    first

---
+++ app.java
@@ -0,0 +1,9 @@
+public class app { 
+    public static void main(String[] args) throws Exception { 
+        System.out.println("Java CI/CD Application Started..."); 
+        while (true) {
+            Thread.sleep(5000); 
+System.out.println("Hello from Java CI/CD Pipeline!");
+        } 
+    } 
+}
